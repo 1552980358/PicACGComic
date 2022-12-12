8dee0e7a1f5cf25281115c69bab151da61fdb24b
1552980358 <1552980358@qq.com>
2022-12-12 11:14:16 +0800

    AccountLoginFragment: Migrate to inherit BaseFragment

--- app/src/main/java/projekt/cloud/piece/pic/ui/account/login/AccountLoginFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/account/login/AccountLoginFragment.kt
@@ -1,31 +1,23 @@
 package projekt.cloud.piece.pic.ui.account.login
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import projekt.cloud.piece.pic.ApplicationConfigs
 import projekt.cloud.piece.pic.R
+import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentAccountLoginBinding
 import projekt.cloud.piece.pic.ui.account.login.login.LoginFragment
 import projekt.cloud.piece.pic.ui.account.login.register.RegisterFragment
+import projekt.cloud.piece.pic.util.FragmentUtil.setSupportActionBar
 
-class AccountLoginFragment: Fragment() {
+class AccountLoginFragment: BaseFragment<FragmentAccountLoginBinding>() {
 
-    private var _binding: FragmentAccountLoginBinding? = null
-    private val binding: FragmentAccountLoginBinding
-        get() = _binding!!
-    private val root get() = binding.root
     private val toolbar: MaterialToolbar
         get() = binding.materialToolbar
     private val tabLayout: TabLayout
@@ -39,22 +31,13 @@ class AccountLoginFragment: Fragment() {
         super.onCreate(savedInstanceState)
         navController = requireParentFragment().findNavController()
     }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentAccountLoginBinding.inflate(inflater, container, false)
-        val applicationConfigs: ApplicationConfigs by viewModels(ownerProducer = { requireActivity() })
-        binding.applicationConfigs = applicationConfigs
-        binding.lifecycleOwner = viewLifecycleOwner
-        return root
+    
+    override fun setUpToolbar() {
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        with(requireActivity() as AppCompatActivity) {
-            setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            toolbar.setNavigationOnClickListener { navController.navigateUp() }
-        }
-
+    
+    override fun setUpViews() {
         binding.viewPager2.adapter = object: FragmentStateAdapter(this) {
             private val fragments = arrayOf(LoginFragment(), RegisterFragment())
             override fun getItemCount() = fragments.size
