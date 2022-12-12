6062f54d46abcb0cd6fbda89faf0eb435864ddd8
1552980358 <1552980358@qq.com>
2022-12-12 11:30:01 +0800

    AccountFragment: Update fragment transition

--- app/src/main/java/projekt/cloud/piece/pic/ui/account/AccountFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/account/AccountFragment.kt
@@ -3,6 +3,7 @@ package projekt.cloud.piece.pic.ui.account
 import android.os.Bundle
 import androidx.fragment.app.commit
 import com.google.android.material.transition.platform.MaterialContainerTransform
+import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.R
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentAccountBinding
@@ -17,14 +18,24 @@ class AccountFragment: BaseFragment<FragmentAccountBinding>() {
     }
     
     override fun setUpViews() {
-        childFragmentManager.commit {
-            replace(
-                R.id.fragment_container_view,
-                when (applicationConfigs.token.value) {
-                    null -> AccountLoginFragment()
-                    else -> AccountDetailFragment()
-                }
-            )
+        applicationConfigs.account.observe(viewLifecycleOwner) {
+    
+            val newFragment = when (it?.token) {
+                null -> AccountLoginFragment()
+                else -> AccountDetailFragment()
+            }
+    
+            childFragmentManager.findFragmentById(R.id.fragment_container_view)?.let { currentFragment ->
+                newFragment.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                currentFragment.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+            }
+            
+            childFragmentManager.commit {
+                replace(
+                    R.id.fragment_container_view,
+                    newFragment
+                )
+            }
         }
     }
     
