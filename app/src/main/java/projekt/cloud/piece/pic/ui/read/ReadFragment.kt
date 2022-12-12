d349cb48eeda08e4b758eef636c8a544c6c9c861
1552980358 <1552980358@qq.com>
2022-12-13 03:04:06 +0800

    ReadFragment: Update order obtaining from argument

--- app/src/main/java/projekt/cloud/piece/pic/ui/read/ReadFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/read/ReadFragment.kt
@@ -1,10 +1,12 @@
 package projekt.cloud.piece.pic.ui.read
 
 import android.os.Bundle
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.google.android.material.transition.platform.MaterialSharedAxis
+import projekt.cloud.piece.pic.ComicDetail
 import projekt.cloud.piece.pic.R
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentReadBinding
@@ -12,7 +14,13 @@ import projekt.cloud.piece.pic.ui.read.content.ComicContentFragment
 
 class ReadFragment: BaseFragment<FragmentReadBinding>() {
     
+    private companion object {
+        const val ARG_ORDER = "order"
+    }
+    
     private val readComic: ReadComic by viewModels()
+    
+    private val comicDetail: ComicDetail by activityViewModels()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +37,9 @@ class ReadFragment: BaseFragment<FragmentReadBinding>() {
         }
     
     override fun setUpViews() {
+        val order = args.getInt(ARG_ORDER)
+        readComic.doc = comicDetail.docList.find { it.order == order }
+        
         updateContentFragment()
         readComic.prev.observe(viewLifecycleOwner) {
             updateContentFragment()
