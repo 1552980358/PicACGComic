6ac29ecbf9093e8e6fab8b787d20c239f722c226
1552980358 <1552980358@qq.com>
2022-12-12 14:19:58 +0800

    FragmentUtil: Add method addMenuProvider()

--- app/src/main/java/projekt/cloud/piece/pic/util/FragmentUtil.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/FragmentUtil.kt
@@ -2,8 +2,10 @@ package projekt.cloud.piece.pic.util
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle.State.STARTED
 
 object FragmentUtil {
 
@@ -12,5 +14,8 @@ object FragmentUtil {
 
     fun Fragment.setSupportActionBar(toolbar: Toolbar) =
         activityAs<AppCompatActivity>().setSupportActionBar(toolbar)
+    
+    fun Fragment.addMenuProvider(menuProvider: MenuProvider) =
+        requireActivity().addMenuProvider(menuProvider, viewLifecycleOwner, STARTED)
 
 }
