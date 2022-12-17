1ba97fccff1dc55135861c6f9165f40904daa36d
1552980358 <1552980358@qq.com>
2022-12-17 15:35:44 +0800

    [Comics] Fix no refresh of list content

--- app/src/main/java/projekt/cloud/piece/pic/Comics.kt
+++ app/src/main/java/projekt/cloud/piece/pic/Comics.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.databinding.ObservableArrayMap
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.withContext
 import projekt.cloud.piece.pic.api.ApiComics.comics
 import projekt.cloud.piece.pic.api.ApiComics.ComicsResponseBody
@@ -43,69 +45,81 @@ class Comics: BaseTaskViewModel() {
     var sort = SORT_DEFAULT_VALUE
         private set
     
+    private var job: Job? = null
+    
     fun changeCategorySort(token: String, sort: ComicsSort) {
         Log.i("Comics", "changeSort category=$key sort=${sort.sortName}")
-        if (this.sort != sort) {
-            comicList.clear()
-            coverImages.clear()
-            page = PAGE_DEFAULT_VALUE
-            hasMorePage = HAS_MORE_PAGE_DEFAULT_VALUE
-            this.sort = sort
-        }
+        this.sort = sort
         requestCategory(token, key)
     }
     
     fun requestCategory(token: String, category: String) {
         Log.i("Comics", "requestCategory category=$category sort=${sort.sortName}")
-        if (key != category) {
-            key = category
+        val oldJob = job
+        job = viewModelScope.ui {
+            oldJob?.cancelAndJoin()
+            clear()
+            if (key != category) {
+                key = category
+            }
+            requestCategoryPageInternal(token)
+            job = null
         }
-        requestCategoryPage(token)
     }
     
     fun requestCategoryPage(token: String) {
-        if (hasMorePage) {
-            Log.i("Comics", "requestCategory category=$key sort=$sort page=${page.plus(1)}")
-            viewModelScope.ui {
-                val httpResponse = withContext(io) {
-                    comics(token, ++page, key, sort.sortName)
-                }
-                val response = httpResponse.response
-                if (httpResponse.code != CodeBook.HTTP_REQUEST_CODE_SUCCESS || response == null) {
-                    return@ui setTaskReceipt(LIST_CODE_ERROR_CONNECTION, httpResponse.message)
-                }
-        
-                if (response.code != HttpUtil.HTTP_RESPONSE_CODE_SUCCESS) {
-                    val errorResponse = withContext(io) {
-                        response.decodeJson<ErrorResponseBody>()
-                    }
-                    return@ui setTaskReceipt(LIST_CODE_ERROR_REJECTED, errorResponse.message)
-                }
-                val comicsResponseBody = withContext(io) {
-                    response.decodeJson<ComicsResponseBody>()
-                }
-                val comics = comicsResponseBody.data.comics
-                val docs = comics.docs
-                io {
-                    docs.forEach {
-                        coverImages[it._id] = it.thumb.bitmap
-                    }
-                }
-                comicList.addAll(docs)
-        
-                setTaskReceipt(LIST_CODE_PART_SUCCESS, null)
-        
-                if (comics.page == comics.pages) {
-                    hasMorePage = false
-                    setTaskReceipt(LIST_CODE_SUCCESS, null)
-                }
+        Log.i("Comics", "requestCategoryPage category=$key sort=${sort.sortName} page=${page.plus(1)}")
+        val oldJob = job
+        job = viewModelScope.ui {
+            oldJob?.join()
+            requestCategoryPageInternal(token)
+            job = null
+        }
+    }
+    
+    private suspend fun requestCategoryPageInternal(token: String) {
+        Log.i("Comics", "requestCategoryPageInternal category=$key sort=${sort.sortName} page=${page.plus(1)}")
+        val httpResponse = withContext(io) {
+            comics(token, ++page, key, sort.sortName)
+        }
+        val response = httpResponse.response
+        if (httpResponse.code != CodeBook.HTTP_REQUEST_CODE_SUCCESS || response == null) {
+            return setTaskReceipt(LIST_CODE_ERROR_CONNECTION, httpResponse.message)
+        }
+    
+        if (response.code != HttpUtil.HTTP_RESPONSE_CODE_SUCCESS) {
+            val errorResponse = withContext(io) {
+                response.decodeJson<ErrorResponseBody>()
             }
+            return setTaskReceipt(LIST_CODE_ERROR_REJECTED, errorResponse.message)
+        }
+        val comicsResponseBody = withContext(io) {
+            response.decodeJson<ComicsResponseBody>()
+        }
+        val comics = comicsResponseBody.data.comics
+        val docs = comics.docs
+        comicList.addAll(docs)
+        setTaskReceipt(LIST_CODE_PART_SUCCESS, null)
+        docs.forEach {
+            coverImages[it._id] = withContext(io) {
+                it.thumb.bitmap
+            }
+        }
+        
+        if (comics.page == comics.pages) {
+            hasMorePage = false
+            setTaskReceipt(LIST_CODE_SUCCESS, null)
         }
     }
     
     override fun clear(lifecycleOwner: LifecycleOwner) {
+        val oldJob = job
+        job = viewModelScope.io {
+            oldJob?.cancelAndJoin()
+            clear()
+            job = null
+        }
         super.clear(lifecycleOwner)
-        clear()
     }
     
     fun clear() {
