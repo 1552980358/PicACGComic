554a75643d52d8ecfbf59b99b14eee77a2cc279b
1552980358 <1552980358@qq.com>
2022-12-19 16:19:41 +0800

    [SearchFragment] Update RecyclerView adapter

--- app/src/main/java/projekt/cloud/piece/pic/ui/search/RecyclerViewAdapter.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/search/RecyclerViewAdapter.kt
@@ -2,81 +2,56 @@ package projekt.cloud.piece.pic.ui.search
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
+import androidx.databinding.ObservableArrayMap
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.withContext
 import projekt.cloud.piece.pic.R
-import projekt.cloud.piece.pic.api.ApiComics.ComicsResponseBody.Data.Comics.Doc
+import projekt.cloud.piece.pic.api.ApiComics.AdvancedSearchResponseBody.Data.Comics.Doc
 import projekt.cloud.piece.pic.databinding.LayoutRecyclerSearchBinding
-import projekt.cloud.piece.pic.util.CoroutineUtil.io
-import projekt.cloud.piece.pic.util.CoroutineUtil.ui
+import projekt.cloud.piece.pic.util.ViewHolderUtil.getString
+import projekt.cloud.piece.pic.util.ViewHolderUtil.setContainerTransitionName
 
-class RecyclerViewAdapter(private val docs: List<Doc>,
-                          private val covers: MutableMap<String, Bitmap?>,
-                          private val onClick: (Doc, View) -> Unit):
-    RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
+class RecyclerViewAdapter(
+    private val comicList: List<Doc>,
+    private val coverImages: ObservableArrayMap<String, Bitmap?>,
+    private val onClick: (View, Doc) -> Unit
+): RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>() {
 
-    inner class RecyclerViewHolder(private val binding: LayoutRecyclerSearchBinding):
-        RecyclerView.ViewHolder(binding.root), OnClickListener {
+    class RecyclerViewHolder(private val binding: LayoutRecyclerSearchBinding):
+        RecyclerView.ViewHolder(binding.root) {
         
         constructor(view: View): this(view.context)
         constructor(context: Context): this(LayoutInflater.from(context))
         constructor(inflater: LayoutInflater): this(LayoutRecyclerSearchBinding.inflate(inflater))
         
-        private var job: Job? = null
-        
-        init {
-            binding.root.setOnClickListener(this)
-        }
-        
-        fun setDoc(doc: Doc) {
-            binding.doc = doc
-            binding.root.transitionName = binding.root.resources.getString(R.string.comic_detail_transition_prefix) + doc._id
-            when {
-                covers.containsKey(doc._id) -> {
-                    binding.bitmap = covers[doc._id]
-                }
-                else -> {
-                    binding.bitmap = BitmapFactory.decodeResource(binding.root.resources, R.drawable.ic_round_image_24)
-                    job?.cancel()
-                    job = ui {
-                        val bitmap = withContext(io) {
-                            doc.thumb.bitmap
-                        }
-                        binding.bitmap = bitmap
-                        covers[doc._id] = bitmap
-                        job = null
-                    }
-                }
-            }
-
-        }
-    
-        override fun onClick(v: View) {
-            binding.doc?.let {
-                onClick.invoke(it, v)
-            }
+        fun bind(comic: Doc, coverImages: ObservableArrayMap<String, Bitmap?>, onClick: (View, Doc) -> Unit) {
+            setContainerTransitionName(getString(R.string.comic_detail_transition_prefix) + comic.id)
+            binding.comic = comic
+            binding.onClick = onClick
+            binding.coverImages = coverImages
         }
         
     }
     
-    private var docSize = docs.size
+    private var docSize = comicList.size
 
     fun notifyListUpdate() {
-        notifyItemRangeInserted(docSize, docs.size - docSize)
-        docSize = docs.size
+        notifyItemRangeInserted(docSize, comicList.size - docSize)
+        docSize = comicList.size
+    }
+    
+    fun notifyListReset() {
+        notifyItemRangeRemoved(docSize, docSize)
+        docSize = comicList.size
     }
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         RecyclerViewHolder(parent)
     
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.setDoc(docs[position])
+        holder.bind(comicList[position], coverImages, onClick)
     }
     
     override fun getItemCount() = docSize
