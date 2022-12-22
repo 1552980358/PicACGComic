5fb355e61b2c09e8cc57cb849e5356a675ae81b0
1552980358 <1552980358@qq.com>
2022-12-23 03:53:25 +0800

    [Home] Update Index

--- app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
@@ -1,5 +1,7 @@
 package projekt.cloud.piece.pic.ui.home
 
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentHomeBinding
 import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
@@ -7,9 +9,13 @@ import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
 class Home: BaseFragment<FragmentHomeBinding>() {
 
     private lateinit var homeLayoutHelper: HomeLayoutHelper
+    private val fragmentContainerView: FragmentContainerView
+        get() = binding.fragmentContainerView
 
     override fun onSetupView(binding: FragmentHomeBinding) {
+        val childNavController = fragmentContainerView.getFragment<NavHostFragment>().navController
         homeLayoutHelper = HomeLayoutHelper(binding, requireActivity().getLayoutSize())
+        homeLayoutHelper.setUpNavigationView(childNavController)
     }
 
 }
---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/index/Index.kt
@@ -0,0 +1,10 @@
+package projekt.cloud.piece.pic.ui.home.index
+
+import projekt.cloud.piece.pic.base.BaseFragment
+import projekt.cloud.piece.pic.databinding.FragmentIndexBinding
+
+class Index: BaseFragment<FragmentIndexBinding>() {
+
+
+
+}
