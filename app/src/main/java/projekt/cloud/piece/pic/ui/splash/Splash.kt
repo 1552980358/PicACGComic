aa13b98bc510faa500fc6bf9e9e22627ed1a1b58
1552980358 <1552980358@qq.com>
2022-12-24 03:39:21 +0800

    [Splash] Update license

--- app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
@@ -1,22 +1,12 @@
 package projekt.cloud.piece.pic.ui.splash
 
-import androidx.navigation.fragment.findNavController
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentSplashBinding
-import projekt.cloud.piece.pic.ui.splash.SplashLayoutHelper.SplashLayout
-import projekt.cloud.piece.pic.ui.splash.SplashLayoutHelper.getSplashLayout
-import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
 
 class Splash: BaseFragment<FragmentSplashBinding>() {
     
-    private lateinit var splashLayout: SplashLayout
+    override fun onSetupView(binding: FragmentSplashBinding) {
     
-    override fun onSetupLayoutHelper(binding: FragmentSplashBinding) {
-        splashLayout = binding.getSplashLayout(requireActivity().getLayoutSize())
-    }
-    
-    override fun onSetupActionBar(binding: FragmentSplashBinding) {
-        splashLayout.setupToolbar(this, findNavController())
     }
     
 }
