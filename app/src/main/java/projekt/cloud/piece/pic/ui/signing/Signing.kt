e8936a001eb88ac564b6d6b832f80def7192efb6
1552980358 <1552980358@qq.com>
2022-12-24 01:47:11 +0800

    [Signing] Update Signing with layout

---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/signing/Signing.kt
@@ -0,0 +1,7 @@
+package projekt.cloud.piece.pic.ui.signing
+
+import projekt.cloud.piece.pic.base.BaseFragment
+import projekt.cloud.piece.pic.databinding.FragmentSigningBinding
+
+class Signing: BaseFragment<FragmentSigningBinding>() {
+}
