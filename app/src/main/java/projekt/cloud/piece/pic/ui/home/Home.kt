01b2cd5d57c44f494cb8a18b93d83c6a594a9b09
1552980358 <1552980358@qq.com>
2022-12-25 20:41:03 +0800

    [Home] Replace HomeLayoutHelper with HomeLayoutCompat

--- app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
@@ -1,21 +1,19 @@
 package projekt.cloud.piece.pic.ui.home
 
-import androidx.fragment.app.FragmentContainerView
-import androidx.navigation.fragment.NavHostFragment
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentHomeBinding
-import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
+import projekt.cloud.piece.pic.ui.home.HomeLayoutCompat.HomeLayoutCompatUtil.getLayoutCompat
 
 class Home: BaseFragment<FragmentHomeBinding>() {
 
-    private lateinit var homeLayoutHelper: HomeLayoutHelper
-    private val fragmentContainerView: FragmentContainerView
-        get() = binding.fragmentContainerView
+    private lateinit var homeLayoutCompat: HomeLayoutCompat
+
+    override fun onSetupLayoutHelper(binding: FragmentHomeBinding) {
+        homeLayoutCompat = binding.getLayoutCompat(requireActivity())
+    }
 
     override fun onSetupView(binding: FragmentHomeBinding) {
-        val childNavController = fragmentContainerView.getFragment<NavHostFragment>().navController
-        homeLayoutHelper = HomeLayoutHelper(binding, requireActivity().getLayoutSize())
-        homeLayoutHelper.setUpNavigationView(childNavController)
+        homeLayoutCompat.setUpNavigationView()
     }
 
 }
---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/HomeLayoutCompat.kt
@@ -0,0 +1,67 @@
+package projekt.cloud.piece.pic.ui.home
+
+import android.app.Activity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigationrail.NavigationRailView
+import projekt.cloud.piece.pic.databinding.FragmentHomeBinding
+import projekt.cloud.piece.pic.databinding.NavRailHeaderHomeBinding
+import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.COMPACT
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.MEDIUM
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.EXPANDED
+
+abstract class HomeLayoutCompat(protected val binding: FragmentHomeBinding) {
+
+    companion object HomeLayoutCompatUtil {
+        fun FragmentHomeBinding.getLayoutCompat(activity: Activity) = when (activity.getLayoutSize()) {
+            COMPACT -> HomeLayoutCompatImpl(this)
+            MEDIUM -> HomeLayoutCompatW600dpImpl(this)
+            EXPANDED -> HomeLayoutCompatW1240dpImpl(this)
+        }
+    }
+
+    private val fragmentContainerView: FragmentContainerView
+        get() = binding.fragmentContainerView
+
+    protected val drawerLayout: DrawerLayout
+        get() = binding.drawerLayout!!
+    private val navigationView: NavigationView
+        get() = binding.navigationView
+
+    protected val navController = fragmentContainerView.getFragment<NavHostFragment>()
+        .navController
+
+    open fun setUpNavigationView() {
+        navigationView.setupWithNavController(navController)
+    }
+
+    private open class HomeLayoutCompatImpl(binding: FragmentHomeBinding): HomeLayoutCompat(binding)
+
+    private class HomeLayoutCompatW600dpImpl(binding: FragmentHomeBinding): HomeLayoutCompat(binding) {
+
+        private val navigationRail: NavigationRailView
+            get() = binding.navigationRail!!
+
+        override fun setUpNavigationView() {
+            super.setUpNavigationView()
+            navigationRail.setupWithNavController(navController)
+            navigationRail.headerView?.let {
+                NavRailHeaderHomeBinding.bind(it)
+                    .appCompatImageButton
+                    .setOnClickListener {
+                        if (!drawerLayout.isOpen) {
+                            drawerLayout.open()
+                        }
+                    }
+            }
+        }
+
+    }
+
+    private class HomeLayoutCompatW1240dpImpl(binding: FragmentHomeBinding): HomeLayoutCompat(binding)
+
+}
