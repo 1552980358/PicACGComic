8a9961f486b5340e6f82f8cfd519ad7f430860eb
1552980358 <1552980358@qq.com>
2022-12-25 20:08:54 +0800

    [LauncherActivity] Update navigation for w600dp an w1240dp device

--- app/src/main/java/projekt/cloud/piece/pic/LauncherActivity.kt
+++ app/src/main/java/projekt/cloud/piece/pic/LauncherActivity.kt
@@ -3,17 +3,68 @@ package projekt.cloud.piece.pic
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import projekt.cloud.piece.pic.databinding.ActivityLauncherBinding
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.COMPACT
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.MEDIUM
+import projekt.cloud.piece.pic.util.LayoutUtil.LayoutSizeMode.EXPANDED
+import projekt.cloud.piece.pic.util.LayoutUtil.getLayoutSize
 
 class LauncherActivity: AppCompatActivity() {
     
     private lateinit var binding: ActivityLauncherBinding
+
+    private open class LauncherLayoutCompat(protected val binding: ActivityLauncherBinding) {
+        open fun setupNavigation(navController: NavController) = Unit
+    }
+
+    private class LauncherLayoutCompatImpl(
+        binding: ActivityLauncherBinding
+    ): LauncherLayoutCompat(binding)
+
+    private open class LauncherLayoutCompatW600dpImpl(
+        binding: ActivityLauncherBinding
+    ): LauncherLayoutCompat(binding) {
+
+        private val navigationView: NavigationView
+            get() = binding.navigationView!!
+
+        override fun setupNavigation(navController: NavController) {
+            navigationView.setupWithNavController(navController)
+        }
+
+    }
+
+    private class LauncherLayoutCompatW1240dpImpl(
+        binding: ActivityLauncherBinding
+    ): LauncherLayoutCompatW600dpImpl(binding)
+
+    private lateinit var navController: NavController
     
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         binding = ActivityLauncherBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+
+        val layoutCompat = binding.getLayoutCompat(getLayoutSize())
+        layoutCompat.setupNavigation(navController)
+    }
+
+    private fun ActivityLauncherBinding.getLayoutCompat(layoutSizeMode: LayoutSizeMode) = when (layoutSizeMode) {
+        COMPACT -> LauncherLayoutCompatImpl(this)
+        MEDIUM -> LauncherLayoutCompatW600dpImpl(this)
+        EXPANDED -> LauncherLayoutCompatW1240dpImpl(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
