275770c990b9ef2558d7a4f4e68461f59872a9e0
1552980358 <1552980358@qq.com>
2022-12-25 19:23:24 +0800

    [Splash] Update navigating to Browsing

--- app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
@@ -1,12 +1,20 @@
 package projekt.cloud.piece.pic.ui.splash
 
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentSplashBinding
 
 class Splash: BaseFragment<FragmentSplashBinding>() {
+
+    private val goForward: MaterialButton
+        get() = binding.materialButtonGoForward
     
     override fun onSetupView(binding: FragmentSplashBinding) {
-    
+        val navController = findNavController()
+        goForward.setOnClickListener {
+            navController.navigate(SplashDirections.toBrowsing())
+        }
     }
     
 }
