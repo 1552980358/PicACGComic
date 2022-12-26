5b86ec67c54dad492728228e8b420b2855995c2d
1552980358 <1552980358@qq.com>
2022-12-26 21:34:42 +0800

    [Signing] Update navigating to SignIn

--- app/src/main/java/projekt/cloud/piece/pic/ui/signing/Signing.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/signing/Signing.kt
@@ -1,7 +1,49 @@
 package projekt.cloud.piece.pic.ui.signing
 
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentSigningBinding
+import projekt.cloud.piece.pic.util.LayoutSizeMode
+import projekt.cloud.piece.pic.util.LayoutSizeMode.COMPACT
 
-class Signing: BaseFragment<FragmentSigningBinding>() {
+class Signing: BaseFragment<FragmentSigningBinding>(), OnClickListener {
+
+    private lateinit var navController: NavController
+
+    private val signIn: MaterialTextView
+        get() = binding.materialTextViewSignIn
+
+    override fun onSetupAnimation(layoutSizeMode: LayoutSizeMode) {
+        when (layoutSizeMode) {
+            COMPACT -> {
+                enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+            }
+            else -> {
+                enterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true)
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true)
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, false)
+            }
+        }
+    }
+
+    override fun onSetupView(binding: FragmentSigningBinding) {
+        navController = findNavController()
+        signIn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            signIn -> {
+                navController.navigate(SigningDirections.toSignIn())
+            }
+        }
+    }
+
 }
