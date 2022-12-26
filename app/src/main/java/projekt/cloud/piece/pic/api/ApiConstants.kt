82951953a3234911851441a1c19d67904ad47d76
1552980358 <1552980358@qq.com>
2022-12-26 16:52:08 +0800

    [ApiConstants] Update constant PICA_COMIC_API_DOMAIN

--- app/src/main/java/projekt/cloud/piece/pic/api/ApiConstants.kt
+++ app/src/main/java/projekt/cloud/piece/pic/api/ApiConstants.kt
@@ -2,6 +2,8 @@ package projekt.cloud.piece.pic.api
 
 object ApiConstants {
 
+    const val PICA_COMIC_API_DOMAIN = "https://picaapi.picacomic.com/"
+
     const val IGNORE_STRING = ""
     const val IGNORE_INT = -1
 
