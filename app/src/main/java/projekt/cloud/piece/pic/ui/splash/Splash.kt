ec9815c588b46f3bea1b0e502146f70ce8e49019
1552980358 <1552980358@qq.com>
2022-12-27 00:49:08 +0800

    [Splash] Update animation

--- app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/splash/Splash.kt
@@ -2,13 +2,20 @@ package projekt.cloud.piece.pic.ui.splash
 
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentSplashBinding
+import projekt.cloud.piece.pic.util.LayoutSizeMode
 
 class Splash: BaseFragment<FragmentSplashBinding>() {
 
     private val goForward: MaterialButton
         get() = binding.materialButtonGoForward
+    
+    override fun onSetupAnimation(layoutSizeMode: LayoutSizeMode) {
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+    }
     
     override fun onSetupView(binding: FragmentSplashBinding) {
         val navController = findNavController()
