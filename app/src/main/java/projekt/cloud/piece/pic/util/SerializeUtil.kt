c330676cca849514d690f3eb7a0a1f43a7db65bc
1552980358 <1552980358@qq.com>
2022-12-26 21:26:36 +0800

    [SerializeUtil] Update `Response.decodeJson`

--- app/src/main/java/projekt/cloud/piece/pic/util/SerializeUtil.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/SerializeUtil.kt
@@ -3,11 +3,14 @@ package projekt.cloud.piece.pic.util
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import okhttp3.Response
 
 object SerializeUtil {
 
     inline fun <reified T> String.decodeJson() = Json.decodeFromString<T>(this)
 
     inline fun <reified T> T.encodeJson() = Json.encodeToString(this)
+
+    inline fun <reified T> Response.decodeJson() = body.string().decodeJson<T>()
 
 }
