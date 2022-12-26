1a10205c36bdce8a780a1914c51cc6400be89ccd
1552980358 <1552980358@qq.com>
2022-12-26 21:34:21 +0800

    [SignIn] Update SignIn with layout

---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/signIn/SignIn.kt
@@ -0,0 +1,114 @@
+package projekt.cloud.piece.pic.ui.signIn
+
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.platform.MaterialSharedAxis
+import kotlinx.coroutines.Job
+import projekt.cloud.piece.pic.R
+import projekt.cloud.piece.pic.api.ErrorResponse.checkRejected
+import projekt.cloud.piece.pic.api.ErrorResponse.decodeErrorResponse
+import projekt.cloud.piece.pic.api.auth.SignIn.SIGN_IN_ERROR_INVALID
+import projekt.cloud.piece.pic.api.auth.SignIn.decodeSignInResponse
+import projekt.cloud.piece.pic.api.auth.SignIn.signIn
+import projekt.cloud.piece.pic.base.BaseFragment
+import projekt.cloud.piece.pic.databinding.FragmentSignInBinding
+import projekt.cloud.piece.pic.ui.signIn.SignInLayoutCompat.SignInLayoutCompatUtil.getLayoutCompat
+import projekt.cloud.piece.pic.util.CoroutineUtil.ui
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestState.STATE_EXCEPTION
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestState.STATE_IO_EXCEPTION
+import projekt.cloud.piece.pic.util.LayoutSizeMode
+import projekt.cloud.piece.pic.util.LayoutSizeMode.COMPACT
+
+class SignIn: BaseFragment<FragmentSignInBinding>() {
+
+    private var job: Job? = null
+
+    override fun onSetupAnimation(layoutSizeMode: LayoutSizeMode) {
+        when (layoutSizeMode) {
+            COMPACT -> {
+                enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+            }
+            else -> {
+                enterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true)
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true)
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, false)
+            }
+        }
+    }
+
+    private lateinit var layoutCompat: SignInLayoutCompat
+
+    override fun onSetupLayoutHelper(binding: FragmentSignInBinding) {
+        layoutCompat = binding.getLayoutCompat(requireActivity())
+        layoutCompat.setNavController(findNavController())
+    }
+
+    override fun onSetupActionBar(binding: FragmentSignInBinding) {
+        layoutCompat.setupActionBar(this)
+    }
+
+    override fun onSetupView(binding: FragmentSignInBinding) {
+        layoutCompat.setCallback { username, password ->
+            invokeSignIn(username, password)
+        }
+    }
+
+    private fun invokeSignIn(username: String, password: String) {
+        if (job?.isActive != true) {
+            job = lifecycleScope.ui {
+                val httpRequest = signIn(username, password)
+
+                if (httpRequest.isComplete) {
+                    val response = httpRequest.response
+
+                    // Error
+                    if (!response.isSuccessful) {
+                        val error = response.decodeErrorResponse()
+                        if (error.error == SIGN_IN_ERROR_INVALID) {
+                            return@ui layoutCompat.shortSnack(
+                                getString(R.string.sign_in_invalid_field)
+                            )
+                        }
+                        return@ui layoutCompat.shortSnack(
+                            getString(R.string.response_error, error.code, error.error, error.message, error.detail)
+                        )
+                    }
+
+                    val responseText = response.body.string()
+
+                    // Rejected
+                    if (responseText.checkRejected()) {
+                        return@ui layoutCompat.shortSnack(getString(R.string.response_rejected))
+                    }
+
+                    // Complete Auth signing in
+                    setFragmentResult(
+                        getString(R.string.auth_sign_in_result),
+                        bundleOf(
+                            getString(R.string.auth_sign_in_result_username) to username,
+                            getString(R.string.auth_sign_in_result_password) to password,
+                            getString(R.string.auth_sign_in_result_token) to responseText.decodeSignInResponse().token,
+                        )
+                    )
+                }
+
+                when (httpRequest.state) {
+                    STATE_IO_EXCEPTION -> {
+                        layoutCompat.shortSnack(getString(R.string.request_io_exception, httpRequest.exceptionMessage))
+                    }
+                    STATE_EXCEPTION -> {
+                        layoutCompat.shortSnack(getString(R.string.request_unknown_exception, httpRequest.exceptionMessage))
+                    }
+                    else -> {
+                        layoutCompat.shortSnack(getString(R.string.request_unexpected_state, httpRequest.state.name))
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/signIn/SignInLayoutCompat.kt
@@ -0,0 +1,96 @@
+package projekt.cloud.piece.pic.ui.signIn
+
+import android.app.Activity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.transition.platform.MaterialSharedAxis
+import projekt.cloud.piece.pic.databinding.FragmentSignInBinding
+import projekt.cloud.piece.pic.util.FragmentUtil.setSupportActionBar
+import projekt.cloud.piece.pic.util.LayoutSizeMode.COMPACT
+import projekt.cloud.piece.pic.util.LayoutSizeMode.MEDIUM
+import projekt.cloud.piece.pic.util.LayoutSizeMode.EXPANDED
+import projekt.cloud.piece.pic.util.LayoutSizeMode.LayoutSizeModeUtil.getLayoutSize
+
+typealias SignInButtonOnClickCallback = (String, String) -> Unit
+
+abstract class SignInLayoutCompat(protected val binding: FragmentSignInBinding) {
+
+    companion object SignInLayoutCompatUtil {
+        @JvmStatic
+        fun FragmentSignInBinding.getLayoutCompat(activity: Activity) = when (activity.getLayoutSize()) {
+            COMPACT -> SignInLayoutCompatImpl(this)
+            MEDIUM -> SignInLayoutCompatW600dpImpl(this)
+            EXPANDED -> SignInLayoutCompatW1240dpImpl(this)
+        }
+    }
+
+    private val coordinatorLayout: CoordinatorLayout
+        get() = binding.coordinatorLayout
+
+    private val username: TextInputLayout
+        get() = binding.textInputLayoutUsername
+    private val password: TextInputLayout
+        get() = binding.textInputLayoutPassword
+
+    private var callback: SignInButtonOnClickCallback? = null
+    private var snackBar: Snackbar? = null
+
+    init {
+        @Suppress("LeakingThis")
+        binding.layoutCompat = this
+    }
+
+    fun setCallback(callback: SignInButtonOnClickCallback) {
+        this.callback = callback
+    }
+
+    fun shortSnack(message: String) {
+        snackBar?.dismiss()
+        snackBar = Snackbar.make(coordinatorLayout, message, LENGTH_SHORT)
+        snackBar?.show()
+    }
+
+    protected lateinit var navController: NavController
+        private set
+
+    fun setNavController(navController: NavController) {
+        this.navController = navController
+    }
+
+    open fun setupActionBar(fragment: Fragment) = Unit
+
+    fun onSignInButtonClick() {
+        val username = username.editText?.text?.toString()
+        val password = password.editText?.text?.toString()
+        if (!username.isNullOrBlank() && !password.isNullOrBlank()) {
+            callback?.invoke(username, password)
+        }
+    }
+
+    private class SignInLayoutCompatImpl(binding: FragmentSignInBinding): SignInLayoutCompat(binding) {
+
+        private val toolbar: MaterialToolbar
+            get() = binding.materialToolbar!!
+
+        override fun setupActionBar(fragment: Fragment) {
+            fragment.setSupportActionBar(toolbar)
+            // toolbar.setupWithNavController(navController)
+            toolbar.title = null
+            toolbar.setNavigationOnClickListener {
+                fragment.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+                navController.navigateUp()
+            }
+        }
+
+    }
+
+    private class SignInLayoutCompatW600dpImpl(binding: FragmentSignInBinding): SignInLayoutCompat(binding)
+
+    private class SignInLayoutCompatW1240dpImpl(binding: FragmentSignInBinding): SignInLayoutCompat(binding)
+
+}
