151366bf1393e140a17e416bbefcdb1b1319d4f9
1552980358 <1552980358@qq.com>
2022-12-27 20:17:50 +0800

    [FragmentUtil] Update method `Fragment.defaultSharedPreference`

--- app/src/main/java/projekt/cloud/piece/pic/util/FragmentUtil.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/FragmentUtil.kt
@@ -1,13 +1,18 @@
 package projekt.cloud.piece.pic.util
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import projekt.cloud.piece.pic.util.ActivityUtil.defaultSharedPreference
 
 object FragmentUtil {
 
     fun Fragment.setSupportActionBar(toolbar: Toolbar) {
         (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
     }
+    
+    val Fragment.defaultSharedPreference: SharedPreferences
+        get() = requireActivity().defaultSharedPreference
 
 }
