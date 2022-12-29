9ca53422211e3a4fe60207629f30306bb765f9b4
1552980358 <1552980358@qq.com>
2022-12-29 21:42:52 +0800

    [BaseApiRequest] Update body caching

--- app/src/main/java/projekt/cloud/piece/pic/api/base/BaseApiRequest.kt
+++ app/src/main/java/projekt/cloud/piece/pic/api/base/BaseApiRequest.kt
@@ -1,6 +1,8 @@
 package projekt.cloud.piece.pic.api.base
 
+import okhttp3.Response
 import projekt.cloud.piece.pic.util.HttpRequest
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestState
 
 abstract class BaseApiRequest {
     
@@ -13,10 +15,26 @@ abstract class BaseApiRequest {
     
     protected lateinit var httpRequest: HttpRequest
     
+    val state: HttpRequestState
+        get() = httpRequest.state
+    
+    val message: String?
+        get() = httpRequest.exceptionMessage
+    
+    protected val isSuccessful: Boolean
+        get() = httpRequest.response.isSuccessful
+    
     protected abstract suspend fun requestApi(): HttpRequest
     
     private suspend fun requestInternal() {
         httpRequest = requestApi()
+        if (httpRequest.isComplete) {
+            httpRequest.response.use { response ->
+                copyStreamData(response)
+            }
+        }
     }
+    
+    protected abstract fun copyStreamData(response: Response)
     
 }
