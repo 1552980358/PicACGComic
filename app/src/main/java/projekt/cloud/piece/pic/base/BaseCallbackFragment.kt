5f712a7c59ffdb37f5893e939db7cb7d18f315ec
1552980358 <1552980358@qq.com>
2022-12-29 21:43:20 +0800

    [BaseCallbackViewModel] Update callback args

--- app/src/main/java/projekt/cloud/piece/pic/base/BaseCallbackFragment.kt
+++ app/src/main/java/projekt/cloud/piece/pic/base/BaseCallbackFragment.kt
@@ -7,12 +7,12 @@ import androidx.viewbinding.ViewBinding
 abstract class BaseCallbackFragment<VB: ViewBinding, VM: BaseCallbackViewModel>: BaseViewModelFragment<VB, VM>() {
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel.registerCallback(viewLifecycleOwner) { code, message ->
-            onCallbackReceived(code, message)
+        viewModel.registerCallback(viewLifecycleOwner) { code, message, responseCode, errorCode, responseDetail ->
+            onCallbackReceived(code, message, responseCode, errorCode, responseDetail)
         }
         super.onViewCreated(view, savedInstanceState)
     }
     
-    protected open fun onCallbackReceived(code: Int, message: String?) = Unit
+    protected open fun onCallbackReceived(code: Int, message: String?, responseCode: Int?, errorCode: Int?, responseDetail: String?) = Unit
     
 }
--- app/src/main/java/projekt/cloud/piece/pic/base/BaseCallbackViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/pic/base/BaseCallbackViewModel.kt
@@ -6,13 +6,28 @@ import androidx.lifecycle.ViewModel
 
 abstract class BaseCallbackViewModel: ViewModel() {
 
-    private class Callback(val code: Int, val message: String?)
+    private class Callback(
+        val code: Int,
+        val message: String?,
+        val responseCode: Int?,
+        val errorCode: Int?,
+        val responseDetail: String?
+    )
+    
     private val _callback = MutableLiveData<Callback>()
     
-    fun registerCallback(lifecycleOwner: LifecycleOwner, callback: (Int, String?) -> Unit) {
+    fun registerCallback(lifecycleOwner: LifecycleOwner, callback: (Int, String?, Int?, Int?, String?) -> Unit) {
         _callback.observe(lifecycleOwner) {
-            callback.invoke(it.code, it.message)
+            callback.invoke(it.code, it.message, it.responseCode, it.errorCode, it.responseDetail)
         }
+    }
+    
+    fun setCallback(code: Int,
+                    message: String? = null,
+                    responseCode: Int? = null,
+                    errorCode: Int? = null,
+                    responseDetail: String? = null) {
+        _callback.value = Callback(code, message, responseCode, errorCode, responseDetail)
     }
 
 }
