5bda1195e4a37989c0a5a1799b34ea810359ad28
1552980358 <1552980358@qq.com>
2023-01-02 02:44:47 +0800

    [copy_pixel.h] Fix `;` missing

--- app/src/main/cpp/bitmap-bundle/copy_pixel.h
+++ app/src/main/cpp/bitmap-bundle/copy_pixel.h
@@ -5,6 +5,6 @@
 
 void copy_pixel32_to_buffer(buffer32_t, buffer32_t, const length_t &);
 
-void copy_pixel16_to_buffer(buffer16_t, buffer16_t, const length_t &)
+void copy_pixel16_to_buffer(buffer16_t, buffer16_t, const length_t &);
 
 #endif //PICACGCOMIC_COPY_PIXEL_H
