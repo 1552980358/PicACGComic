2257a54bf6e3f1aff325ff643cefc69ee128f5f9
1552980358 <1552980358@qq.com>
2023-01-02 19:54:17 +0800

    [Home] Add animations

--- app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/Home.kt
@@ -1,5 +1,6 @@
 package projekt.cloud.piece.pic.ui.home
 
+import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.base.BaseFragment
 import projekt.cloud.piece.pic.databinding.FragmentHomeBinding
 import projekt.cloud.piece.pic.ui.home.HomeLayoutCompat.HomeLayoutCompatUtil.getLayoutCompat
@@ -8,6 +9,11 @@ import projekt.cloud.piece.pic.util.LayoutSizeMode
 class Home: BaseFragment<FragmentHomeBinding>() {
 
     private lateinit var homeLayoutCompat: HomeLayoutCompat
+    
+    override fun onSetupAnimation(layoutSizeMode: LayoutSizeMode) {
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+    }
 
     override fun onSetupLayoutCompat(binding: FragmentHomeBinding, layoutSizeMode: LayoutSizeMode) {
         homeLayoutCompat = binding.getLayoutCompat(layoutSizeMode)
