155e009a2367e061e7e8d633558e6cac5c39aaef
1552980358 <1552980358@qq.com>
2023-01-02 20:08:27 +0800

    [Metadata] Update Metadata with layout

---
+++ app/src/main/java/projekt/cloud/piece/pic/ui/comic/metadata/Metadata.kt
@@ -0,0 +1,8 @@
+package projekt.cloud.piece.pic.ui.comic.metadata
+
+import projekt.cloud.piece.pic.base.BaseCallbackFragment
+import projekt.cloud.piece.pic.databinding.FragmentMetadataBinding
+
+class Metadata: BaseCallbackFragment<FragmentMetadataBinding, MetadataViewModel>() {
+
+}
