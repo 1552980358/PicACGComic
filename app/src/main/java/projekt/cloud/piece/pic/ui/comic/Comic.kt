600785bbc50cfeb0de8180109d873683fe52c317
1552980358 <1552980358@qq.com>
2023-01-03 21:42:56 +0800

    [Comic] Update comic metadata requesting

--- app/src/main/java/projekt/cloud/piece/pic/ui/comic/Comic.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/comic/Comic.kt
@@ -1,18 +1,18 @@
 package projekt.cloud.piece.pic.ui.comic
 
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.transition.platform.MaterialSharedAxis
 import projekt.cloud.piece.pic.MainViewModel
 import projekt.cloud.piece.pic.R
-import projekt.cloud.piece.pic.base.BaseFragment
+import projekt.cloud.piece.pic.base.BaseCallbackFragment
 import projekt.cloud.piece.pic.databinding.FragmentComicBinding
 import projekt.cloud.piece.pic.ui.comic.ComicLayoutCompat.ComicLayoutCompatUtil.getLayoutCompat
-import projekt.cloud.piece.pic.util.BitmapBundle.Companion.getBitmap
+import projekt.cloud.piece.pic.util.BitmapBundle.Companion.getBitmapAndClose
 import projekt.cloud.piece.pic.util.LayoutSizeMode
 
-class Comic: BaseFragment<FragmentComicBinding>() {
+class Comic: BaseCallbackFragment<FragmentComicBinding, ComicViewModel>() {
     
     private val mainViewModel: MainViewModel by activityViewModels()
     
@@ -24,26 +24,35 @@ class Comic: BaseFragment<FragmentComicBinding>() {
         reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
     }
     
-    override fun onBindData(binding: FragmentComicBinding) {
-        binding.mainViewModel = mainViewModel
-    }
-    
     override fun onSetupLayoutCompat(binding: FragmentComicBinding, layoutSizeMode: LayoutSizeMode) {
         layoutCompat = binding.getLayoutCompat(layoutSizeMode)
         layoutCompat.setNavController(findNavController())
         layoutCompat.setupNavigation(this)
-        
-        val comicViewModel = ViewModelProvider(
-            layoutCompat.childNavController.getViewModelStoreOwner(R.id.nav_graph_comic)
-        )[ComicViewModel::class.java]
-        
-        with(comicViewModel) {
-            val arguments = requireArguments()
-            setCover(arguments.getBitmap(getString(R.string.comic_arg_cover)))
-            setId(arguments.getString(getString(R.string.comic_arg_id)))
-            setTitle(arguments.getString(getString(R.string.comic_arg_title)))
+    }
+    
+    override fun onBindData(binding: FragmentComicBinding) {
+        binding.mainViewModel = mainViewModel
+    
+        val arguments = requireArguments()
+        val id = arguments.getString(getString(R.string.comic_arg_id))
+        if (id != null && viewModel.id.value != id) {
+            viewModel.setId(id)
+            viewModel.setCover(arguments.getBitmapAndClose(getString(R.string.comic_arg_cover)))
+            viewModel.setTitle(arguments.getString(getString(R.string.comic_arg_title)))
+            mainViewModel.account.observe(viewLifecycleOwner) {
+                when {
+                    it.isSignedIn -> startRequestComicMetadata(it.token, id)
+                    else -> mainViewModel.performSignIn(requireActivity())
+                }
+            }
         }
-        
     }
 
+    private fun startRequestComicMetadata(token: String, id: String) {
+        viewModel.scopedRequestComicMetadata(lifecycleScope, token, id)
+    }
+    
+    override fun onCallbackReceived(code: Int, message: String?, responseCode: Int?, errorCode: Int?, responseDetail: String?) {
+    }
+    
 }
--- app/src/main/java/projekt/cloud/piece/pic/ui/comic/ComicViewModel.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/comic/ComicViewModel.kt
@@ -3,9 +3,28 @@ package projekt.cloud.piece.pic.ui.comic
 import android.graphics.Bitmap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import projekt.cloud.piece.pic.api.base.BaseApiRequest.Companion.request
+import projekt.cloud.piece.pic.api.comics.metadata.ComicMetadata
+import projekt.cloud.piece.pic.api.comics.metadata.ComicMetadataResponseBody.Comic
+import projekt.cloud.piece.pic.api.comics.metadata.ComicMetadataResponseBody.Creator
+import projekt.cloud.piece.pic.base.BaseCallbackViewModel
+import projekt.cloud.piece.pic.util.CoroutineUtil.ui
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestState.STATE_IO_EXCEPTION
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestState.STATE_EXCEPTION
 
-class ComicViewModel: ViewModel() {
+class ComicViewModel: BaseCallbackViewModel() {
+    
+    companion object MetadataCallbackCode {
+        const val METADATA_COMPLETE = 0
+        const val METADATA_IO_EXCEPTION = -1
+        const val METADATA_EXCEPTION = -2
+        const val METADATA_INVALID_STATE_CODE = -3
+        const val METADATA_ERROR = -4
+        const val METADATA_EMPTY_CONTENT = -5
+        const val METADATA_REJECTED = -6
+    }
 
     private val _cover = MutableLiveData<Bitmap?>()
     val cover: LiveData<Bitmap?>
@@ -26,6 +45,69 @@ class ComicViewModel: ViewModel() {
         get() = _title
     fun setTitle(title: String?) {
         _title.value = title
+    }
+    
+    private val _comic = MutableLiveData<Comic?>()
+    val comic: LiveData<Comic?>
+        get() = _comic
+    
+    private val _creator = MutableLiveData<Creator?>()
+    val creator: LiveData<Creator?>
+        get() = _creator
+    
+    private val _creatorAvatar = MutableLiveData<Bitmap?>()
+    val creatorAvatar: MutableLiveData<Bitmap?>
+        get() = _creatorAvatar
+    
+    fun scopedRequestComicMetadata(scope: CoroutineScope = viewModelScope, token: String, id: String) {
+        if (_comic.value?.id != id) {
+            _comic.value = null
+            _creator.value = null
+            _creatorAvatar.value = null
+            
+            scope.ui {
+                val metadata = ComicMetadata(token, id).request()
+                if (!metadata.isComplete) {
+                    return@ui when (metadata.state) {
+                        STATE_IO_EXCEPTION -> {
+                            setCallback(METADATA_IO_EXCEPTION, metadata.message)
+                        }
+                        STATE_EXCEPTION -> {
+                            setCallback(METADATA_EXCEPTION, metadata.message)
+                        }
+                        else -> {
+                            setCallback(METADATA_INVALID_STATE_CODE, metadata.message)
+                        }
+                    }
+                }
+                if (metadata.isErrorResponse) {
+                    return@ui metadata.errorResponse().let { errorResponseBody ->
+                        setCallback(
+                            METADATA_ERROR,
+                            errorResponseBody.message,
+                            errorResponseBody.code,
+                            errorResponseBody.error,
+                            errorResponseBody.detail
+                        )
+                    }
+                }
+                
+                if (metadata.isEmptyResponse) {
+                    return@ui setCallback(METADATA_EMPTY_CONTENT)
+                }
+                
+                if (metadata.isRejected()) {
+                    return@ui setCallback(METADATA_REJECTED)
+                }
+                
+                val responseBody = metadata.responseBody()
+                _comic.value = responseBody.comic
+                _creator.value = responseBody.creator
+                _creatorAvatar.value = responseBody.creator.avatar.obtainBitmap()
+                
+                setCallback(METADATA_COMPLETE)
+            }
+        }
     }
 
 }
