7d675fee58ddef5e01a1d8097cf599c559769a5e
1552980358 <1552980358@qq.com>
2023-01-11 19:29:37 +0800

    [DataBinding] update methods

--- app/src/main/java/projekt/cloud/piece/pic/util/DataBinding.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/DataBinding.kt
@@ -1,6 +1,7 @@
 package projekt.cloud.piece.pic.util
 
 import android.graphics.Bitmap
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 
@@ -10,6 +11,39 @@ object DataBinding {
     @BindingAdapter("bitmap")
     fun AppCompatImageView.setBitmap(bitmap: Bitmap?) {
         setImageBitmap(bitmap)
+    }
+    
+    @JvmStatic
+    @BindingAdapter("android:paddingStart")
+    fun View.setPaddingStart(paddingStart: Int) {
+        setPadding(
+            paddingStart,
+            paddingTop,
+            paddingRight,
+            paddingBottom
+        )
+    }
+    
+    @JvmStatic
+    @BindingAdapter("android:paddingEnd")
+    fun View.setPaddingEnd(paddingEnd: Int) {
+        setPadding(
+            paddingStart,
+            paddingTop,
+            paddingEnd,
+            paddingBottom
+        )
+    }
+    
+    @JvmStatic
+    @BindingAdapter("android:paddingHorizontal")
+    fun View.setPaddingHorizontal(paddingHorizontal: Int) {
+        setPadding(
+            paddingHorizontal,
+            paddingTop,
+            paddingHorizontal,
+            paddingBottom
+        )
     }
     
 }
