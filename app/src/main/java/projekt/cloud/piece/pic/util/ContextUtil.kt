4ff0f6cfb7c2400111ed92e82608b86c0e9993dd
1552980358 <1552980358@qq.com>
2023-01-11 19:29:21 +0800

    [ContextUtil] Update method `Context.dpToPx`

--- app/src/main/java/projekt/cloud/piece/pic/util/ContextUtil.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/ContextUtil.kt
@@ -3,11 +3,15 @@ package projekt.cloud.piece.pic.util
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import projekt.cloud.piece.pic.util.ResourceUtil.dpToPx
 
 object ContextUtil {
     
     @JvmStatic
     val Context.defaultSharedPreference: SharedPreferences
         get() = PreferenceManager.getDefaultSharedPreferences(this)
+    
+    @JvmStatic
+    fun Context.dpToPx(dp: Int) = resources.dpToPx(dp)
     
 }
