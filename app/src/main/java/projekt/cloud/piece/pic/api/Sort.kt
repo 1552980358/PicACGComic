a68dc7e478dcec015ea4ab6f6f1ff041e94cf69a
1552980358 <1552980358@qq.com>
2023-01-13 16:04:08 +0800

    [Sort] Fix incorrect sort string

--- app/src/main/java/projekt/cloud/piece/pic/api/Sort.kt
+++ app/src/main/java/projekt/cloud/piece/pic/api/Sort.kt
@@ -3,6 +3,6 @@ package projekt.cloud.piece.pic.api
 enum class Sort(val string: String) {
     NEW_TO_OLD("dd"),
     OLD_TO_NEW("da"),
-    MORE_HEART("lv"),
+    MORE_HEART("ld"),
     MORE_FAVOURITE("vd");
 }
