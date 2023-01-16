7ad1a75c4746164b8810da01dddf4a5846682e7f
1552980358 <1552980358@qq.com>
2023-01-16 19:03:46 +0800

    [LikeResponseBody] Fix incorrect type of arg `message`

--- app/src/main/java/projekt/cloud/piece/pic/api/comics/like/LikeResponseBody.kt
+++ app/src/main/java/projekt/cloud/piece/pic/api/comics/like/LikeResponseBody.kt
@@ -3,7 +3,7 @@ package projekt.cloud.piece.pic.api.comics.like
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LikeResponseBody(val code: Int, val message: Int, private val data: Data) {
+data class LikeResponseBody(val code: Int, val message: String, private val data: Data) {
     
     private companion object {
         const val ACTION_LIKE = "like"
