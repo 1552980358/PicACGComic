6fbccd0fdb82ff286535c46d2bb68dd8ac0c8e74
1552980358 <1552980358@qq.com>
2023-01-17 22:13:59 +0800

    [Image] Remove redundant method `obtainBitmap`

--- app/src/main/java/projekt/cloud/piece/pic/api/image/Image.kt
+++ app/src/main/java/projekt/cloud/piece/pic/api/image/Image.kt
@@ -1,11 +1,6 @@
 package projekt.cloud.piece.pic.api.image
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
-import projekt.cloud.piece.pic.util.CoroutineUtil.io
-import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.getRequest
 
 @Serializable
 data class Image(val originalName: String, private val path: String, private val fileServer: String) {
@@ -22,22 +17,6 @@ data class Image(val originalName: String, private val path: String, private val
     
     fun getUrl(): String {
         return domain + STATIC_IMAGE_CONNECTOR + path
-    }
-    
-    suspend fun obtainBitmap(): Bitmap? {
-        return withContext(io) {
-            getRequest(domain, STATIC_IMAGE_CONNECTOR + path).let { httpRequest ->
-                if (!httpRequest.isComplete) {
-                    return@let null
-                }
-                httpRequest.response.use { response ->
-                    if (!response.isSuccessful) {
-                        return@use null
-                    }
-                    BitmapFactory.decodeStream(response.body.byteStream())
-                }
-            }
-        }
     }
     
 }
