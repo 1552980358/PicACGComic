4d13a351c4dca871266abe97293ad75f1e7c8a6d
1552980358 <1552980358@qq.com>
2023-01-18 20:45:36 +0800

    [build.gradle.kts] Update `androidx.test`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.4.1")
     kapt("androidx.databinding:databinding-compiler-common:7.4.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
