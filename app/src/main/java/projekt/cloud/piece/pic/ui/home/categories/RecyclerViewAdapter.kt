bc108f21d1064b9552867d2b676d89e189c34409
1552980358 <1552980358@qq.com>
2023-01-25 09:02:20 +0800

    [RecyclerViewAdapter] Optimize category image

--- app/src/main/java/projekt/cloud/piece/pic/ui/home/categories/RecyclerViewAdapter.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/home/categories/RecyclerViewAdapter.kt
@@ -1,13 +1,21 @@
 package projekt.cloud.piece.pic.ui.home.categories
 
+import android.graphics.drawable.Drawable
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import projekt.cloud.piece.pic.api.comics.categories.CategoriesResponseBody.Category
+import projekt.cloud.piece.pic.api.image.Image
 import projekt.cloud.piece.pic.base.BaseRecyclerViewAdapter
 import projekt.cloud.piece.pic.base.BaseRecyclerViewHolder
 import projekt.cloud.piece.pic.databinding.LayoutRecyclerCategoriesBinding
 import projekt.cloud.piece.pic.ui.home.categories.RecyclerViewAdapter.RecyclerViewHolder
+import projekt.cloud.piece.pic.util.CoroutineUtil.ui
 import projekt.cloud.piece.pic.widget.FixRatioImageView
 
 class RecyclerViewAdapter(
@@ -17,7 +25,7 @@ class RecyclerViewAdapter(
 ): BaseRecyclerViewAdapter<RecyclerViewHolder, Category>(categoryList) {
     
     class RecyclerViewHolder(
-        parent: ViewGroup, private val fragment: Fragment, onClick: (String) -> Unit
+        parent: ViewGroup, onClick: (String) -> Unit
     ): BaseRecyclerViewHolder<LayoutRecyclerCategoriesBinding>(parent, LayoutRecyclerCategoriesBinding::class.java) {
         
         init {
@@ -27,20 +35,42 @@ class RecyclerViewAdapter(
         private val fixRatioImageView: FixRatioImageView
             get() = binding.fixRatioImageView
         
-        fun onBind(category: Category) {
+        fun onBind(category: Category, fragment: Fragment) {
             binding.title = category.title
-            Glide.with(fragment)
-                .load(category.thumb.getUrl())
-                .into(fixRatioImageView)
+            loadImage(category.thumb, fragment)
+        }
+        
+        private fun loadImage(thumb: Image, fragment: Fragment) {
+            fragment.lifecycleScope.ui {
+                Glide.with(fragment)
+                    .load(thumb.getUrl())
+                    .listener(
+                        object: RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean
+                            ): Boolean {
+                                Glide.with(fragment).clear(fixRatioImageView)
+                                loadImage(thumb, fragment)
+                                return false
+                            }
+                            override fun onResourceReady(
+                                resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean
+                            ): Boolean {
+                                return false
+                            }
+                        }
+                    )
+                    .into(fixRatioImageView)
+            }
         }
         
     }
     
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        RecyclerViewHolder(parent, fragment, onClick)
+        RecyclerViewHolder(parent, onClick)
     
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.onBind(itemList[position])
+        holder.onBind(itemList[position], fragment)
     }
     
 }
