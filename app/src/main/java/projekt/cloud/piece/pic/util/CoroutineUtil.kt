ec95b3d2cfedf6272d5c413de651bebe05033f54
1552980358 <1552980358@qq.com>
2023-02-01 07:45:42 +0800

    [CoroutineUtil] Update Default Dispatcher

--- app/src/main/java/projekt/cloud/piece/pic/util/CoroutineUtil.kt
+++ app/src/main/java/projekt/cloud/piece/pic/util/CoroutineUtil.kt
@@ -9,19 +9,26 @@ object CoroutineUtil {
 
     val io = Dispatchers.IO
     val ui = Dispatchers.Main
+    val default = Dispatchers.Default
 
     fun io(block: suspend CoroutineScope.() -> Unit) =
         CoroutineScope(io).launch(block = block)
 
     fun ui(block: suspend CoroutineScope.() -> Unit) =
         CoroutineScope(ui).launch(block = block)
+    
+    fun default(block: suspend CoroutineScope.() -> Unit) =
+        CoroutineScope(default).launch(block = block)
 
     fun CoroutineScope.io(block: suspend CoroutineScope.() -> Unit) =
         launch(io, block = block)
 
     fun CoroutineScope.ui(block: suspend CoroutineScope.() -> Unit) =
         launch(ui, block = block)
-
+    
+    fun CoroutineScope.default(block: suspend CoroutineScope.() -> Unit) =
+        CoroutineScope(default).launch(block = block)
+    
     suspend fun <T> ioContext(block: suspend CoroutineScope.() -> T) =
         withContext(io, block)
 
