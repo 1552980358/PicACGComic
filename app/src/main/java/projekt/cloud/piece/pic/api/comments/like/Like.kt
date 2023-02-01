2fa54861616c33a0f8203bfcfecb1052486367e2
1552980358 <1552980358@qq.com>
2023-02-01 11:17:40 +0800

    [comments/Like] Update api for liking comment

---
+++ app/src/main/java/projekt/cloud/piece/pic/api/comments/like/Like.kt
@@ -0,0 +1,38 @@
+package projekt.cloud.piece.pic.api.comments.like
+
+import kotlinx.coroutines.withContext
+import projekt.cloud.piece.pic.api.ApiConstants.PICA_COMIC_API_DOMAIN
+import projekt.cloud.piece.pic.api.Header
+import projekt.cloud.piece.pic.api.base.BaseStringApiRequest
+import projekt.cloud.piece.pic.api.common.LikeResponseBody
+import projekt.cloud.piece.pic.util.CoroutineUtil.default
+import projekt.cloud.piece.pic.util.CoroutineUtil.io
+import projekt.cloud.piece.pic.util.HttpRequest
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestMethod.POST
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.postJsonRequest
+
+class Like(private val token: String, val id: String): BaseStringApiRequest<LikeResponseBody>() {
+    
+    private companion object {
+        const val API_LIKE_PREFIX = "comments/"
+        const val API_LIKE_SUFFIX = "/like"
+        const val API_LIKE_REQUEST_BODY = "{}"
+    }
+    
+    override suspend fun requestApi(): HttpRequest {
+        val path = API_LIKE_PREFIX + id + API_LIKE_SUFFIX
+        return withContext(io) {
+            postJsonRequest(
+                PICA_COMIC_API_DOMAIN,
+                path,
+                Header.getHeader(path, POST, token),
+                API_LIKE_REQUEST_BODY
+            )
+        }
+    }
+    
+    override suspend fun responseBody(): LikeResponseBody {
+        return withContext(default) { reflectInline(responseBody) }
+    }
+    
+}
