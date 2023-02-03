eba24b4238225954a1b0d21d70d93a6d4e7cd6e2
1552980358 <1552980358@qq.com>
2023-02-04 02:43:57 +0800

    [Comments] Fix navigation back button cannot return to Home

--- app/src/main/java/projekt/cloud/piece/pic/ui/comic/comments/CommentsLayoutCompat.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/comic/comments/CommentsLayoutCompat.kt
@@ -134,7 +134,8 @@ abstract class CommentsLayoutCompat private constructor(
         override fun setupActionBar(fragment: Fragment) {
             fragment.setSupportActionBar(toolbar)
             toolbar.setNavigationOnClickListener {
-                fragment.requireActivity().onBackPressedDispatcher.onBackPressed()
+                fragment.findParentAs<Comic>().findNavController()
+                    .navigateUp()
             }
         }
     
