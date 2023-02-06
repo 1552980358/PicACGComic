b36e9256dff681cce0723af7865595317dac6ad4
1552980358 <1552980358@qq.com>
2023-02-06 17:13:09 +0800

    [Comments] Update replying comment

---
+++ app/src/main/java/projekt/cloud/piece/pic/api/comments/Comments.kt
@@ -0,0 +1,44 @@
+package projekt.cloud.piece.pic.api.comments
+
+import kotlinx.coroutines.withContext
+import projekt.cloud.piece.pic.api.ApiConstants.PICA_COMIC_API_DOMAIN
+import projekt.cloud.piece.pic.api.Header
+import projekt.cloud.piece.pic.api.base.BaseStringApiRequest
+import projekt.cloud.piece.pic.api.common.CommentsRequestBody
+import projekt.cloud.piece.pic.api.common.CommentsResponseBody
+import projekt.cloud.piece.pic.util.CoroutineUtil.default
+import projekt.cloud.piece.pic.util.CoroutineUtil.io
+import projekt.cloud.piece.pic.util.HttpRequest
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.postJsonRequest
+import projekt.cloud.piece.pic.util.HttpRequest.HttpRequestUtil.HttpRequestMethod.POST
+import projekt.cloud.piece.pic.util.SerializeUtil.encodeJson
+
+class Comments(private val token: String, private val id: String, private val content: String): BaseStringApiRequest<CommentsResponseBody>() {
+    
+    private companion object {
+        const val API_COMMENTS = "comments/"
+    }
+    
+    override suspend fun requestApi(): HttpRequest {
+        val path = API_COMMENTS + id
+        return withContext(io) {
+            postJsonRequest(
+                PICA_COMIC_API_DOMAIN,
+                path,
+                Header.getHeader(path, POST, token),
+                jsonRequestBody()
+            )
+        }
+    }
+    
+    private suspend fun jsonRequestBody(): String {
+        return withContext(default) {
+            CommentsRequestBody(content).encodeJson()
+        }
+    }
+    
+    override suspend fun responseBody(): CommentsResponseBody {
+        return withContext(default) { reflectInline(responseBody) }
+    }
+    
+}
