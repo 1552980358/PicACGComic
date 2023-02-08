1f555a62d3a1434a9d9006273fe75b5bf03b07ab
1552980358 <1552980358@qq.com>
2023-02-08 15:48:42 +0800

    [Commenting] Fix incorrect dialog UI

--- app/src/main/java/projekt/cloud/piece/pic/ui/commenting/Commenting.kt
+++ app/src/main/java/projekt/cloud/piece/pic/ui/commenting/Commenting.kt
@@ -1,15 +1,20 @@
 package projekt.cloud.piece.pic.ui.commenting
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import projekt.cloud.piece.pic.MainViewModel
 import projekt.cloud.piece.pic.R
 import projekt.cloud.piece.pic.databinding.DialogFragmentCommentingBinding
 import projekt.cloud.piece.pic.ui.commenting.CommentingLayoutCompat.CommentingLayoutCompatUtil.getLayoutCompat
+import projekt.cloud.piece.pic.util.LayoutSizeMode
+import projekt.cloud.piece.pic.util.LayoutSizeMode.COMPACT
 import projekt.cloud.piece.pic.util.LayoutSizeMode.LayoutSizeModeUtil.getLayoutSize
 
 class Commenting: DialogFragment() {
@@ -18,9 +23,12 @@ class Commenting: DialogFragment() {
     private val binding: DialogFragmentCommentingBinding
         get() = _binding!!
     
+    private lateinit var layoutSizeMode: LayoutSizeMode
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NO_TITLE, R.style.Theme_DialogFragment_Commenting)
+        setStyle(STYLE_NORMAL, R.style.Theme_Pic_Commenting)
+        layoutSizeMode = requireActivity().getLayoutSize()
     }
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -34,6 +42,15 @@ class Commenting: DialogFragment() {
             layoutCompat.setupActionBar(this)
             layoutCompat.setupWithArgument(this, requireArguments())
             layoutCompat.setupMenu(this)
+        }
+    }
+    
+    override fun onStart() {
+        super.onStart()
+        if (layoutSizeMode == COMPACT) {
+            dialog?.window?.let {
+                WindowCompat.setDecorFitsSystemWindows(it, false)
+            }
         }
     }
     
